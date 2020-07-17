402a626303b5b5498f42e7b677537bbb4d2f9161
Edarcega <edimar.eds@gmail.com>
2020-07-17 14:56:31 -0300

    Primeiras linhas

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,5 +1,18 @@
 package entities;
 
 public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
 
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		balance -= amount;
+	}
+	
 }
