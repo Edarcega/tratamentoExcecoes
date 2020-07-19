5e2e3ffbb4195120480babad7cf77171b6f26b5f
Edarcega <edimar.eds@gmail.com>
2020-07-19 16:00:14 -0300

    Classes criadas

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -7,6 +7,34 @@ public class Account {
 	private Double balance;
 	private Double withdrawLimit;
 
+	
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+	
+	public Account () {
+		
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
 	public void deposit(Double amount) {
 		balance += amount;
 	}
